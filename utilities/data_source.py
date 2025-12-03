e9f51ddd9bf697468c8b094c2be078a9ef372686
pcq5384 <prithwi.chaudhury@springernature.com>
2025-12-03 11:58:12 +0530

    modified files

---
+++ utilities/data_source.py
@@ -0,0 +1,6 @@
+class DataSource:
+    data_invalid_login = [("John", "John123", "Invalid credentials"),
+                          ("Saul", "Saul123", "Invalid credentials")]
+
+
+    data_valid_login = [("Admin","admin123")]
